4401fcf7acbb996b6a88539918865a31f5c5bf30
Mateus Correia <correia.mateus@hotmail.com>
2024-03-28 12:48:51 -0300

    4

---
+++ 4.sql
@@ -0,0 +1,13 @@
+-- SQLite
+
+INSERT INTO
+    CAR_BRANDS(BRAND_NAME)
+VALUES
+    ('Chevrolet'),
+    ('Toyota'),
+    ('Hyundai'),
+    ('Volkswagen'),
+    ('Jeep'),
+    ('Renault'),
+    ('Honda'),
+    ('Fiat');
